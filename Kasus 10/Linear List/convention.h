14a44cd5092413baa077a385f89dd582cd0107da
Belva Abthal Hidayat <belva.a.hidayat@gmail.com>
2025-05-22 20:00:52 +0700

    Add files via upload

---
+++ Kasus 10/Linear List/convention.h
@@ -0,0 +1,12 @@
+#ifndef CONVENTION_H
+#define CONVENTION_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//boolean
+#define true 1
+#define false 0
+typedef int boolean;
+
+#endif
